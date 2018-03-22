37261cb8be6ad371d89953cba12b512b2fac833b
CathalRyan96 <cathalryan48@gmail.com>
2018-03-22 10:57:52 +0000

    Made connection to database!

--- src/DAO/DataConnect.java
+++ src/DAO/DataConnect.java
@@ -2,30 +2,50 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DataConnect {
 	
-	public Connection getConnection() {
+	private Connection con;
+	private Statement st;
+	private ResultSet rs;
+	
+	//Method used to connect to SQL Database
+	public DataConnect() {
 		try {
+			
 			Class.forName("com.mysql.jdbc.Driver");
-			Connection conn;
-			try {
-				conn = DriverManager.getConnection("jdbc://mysql://localhost:3306/dbfitness","root","cathal");
-				return conn;
-			} catch (SQLException e) {
+			
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/dbfitness","root","");
+			
+			st = con.createStatement();
+			
+		}catch(Exception ex) {
+			System.out.println("Error: "+ ex);
+		}
+	}
+	
+	//Method for pulling data from the database
+	public void getData() {
+		try {
+			String query = "select * from users";
+			rs = st.executeQuery(query);			
+			System.out.println("Info from database");
+			
+			while(rs.next()) {
+				int uid = rs.getInt("uid");
+				System.out.println("Uid: "+ uid);
+
 				
-				e.printStackTrace();
-				return null;
+				String uname = rs.getString("uname");
+				System.out.println("Uname: "+ uname);
+				
 			}
 			
-		} catch (ClassNotFoundException e) {
-			
-			e.printStackTrace();
-			return null;
+		}catch(Exception ex) {
+			System.out.println(ex);
 		}
-		
-		
 	}
 
 }
--- src/DAO/LoginDAO.java
+++ src/DAO/LoginDAO.java
@@ -1,75 +1,12 @@
 package DAO;
 
-import java.sql.Connection;
-
-
-
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-
-import DAO.DataConnect;
-import managedBeans.Login;
 
 public class LoginDAO {
-	DataConnect dc = new DataConnect();
-
-    Connection conn = dc.getConnection();
-
-    if(conn == null){
-
-      out.println("connection failed!"); 
-
-    }else{   
-
-      String query = "SELECT firstname FROM users where user_id = 0000000035";
-
- 
-
-      // create the java statement
-
-      Statement st = conn.createStatement();
-
-     
-
-      // execute the query, and get a java resultset
-
-      ResultSet rs = st.executeQuery(query);
-
-     
-
-      // iterate through the java resultset
-
-      while (rs.next()){
-
-        String firstName = rs.getString("firstname");
-
-        out.print("\nQuery returns the firstname: \n"+firstName);
-
-      }
-
-     
-
-      st.close();   
-
-    }
-}
-}
-
-
-
-
 	
+	public static void main(String[] args) {
+		DataConnect connect = new DataConnect();
+		connect.getData();
+	}//main
 	
-			
-		
-	
-
-
+}//class
+    

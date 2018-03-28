af8831f8276fc4c6dc8ac0a950f863a5dfe74f02
CathalRyan96 <cathalryan48@gmail.com>
2018-03-28 11:40:38 +0100

    added hurling table in sql database

--- dbfitness.sql
+++ dbfitness.sql
@@ -3,3 +3,11 @@ uid int(20) NOT NULL AUTO_INCREMENT,
 uname VARCHAR(60) NOT NULL, 
 password VARCHAR(60) NOT NULL, 
 PRIMARY KEY(uid));
+
+
+CREATE TABLE hurling (
+  hurling_id INT NOT NULL,
+  hurling_ex VARCHAR(200) NOT NULL,
+  PRIMARY KEY (hurling_id),
+  UNIQUE (hurling_ex)
+);
